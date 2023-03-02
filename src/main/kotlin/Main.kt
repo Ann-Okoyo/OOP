bbc1e09e1c43c09a39e41d5ba332009db8592b70
Muyale Okoyo <annmuyale@gmail.com>
2023-03-02 08:20:39 +0300

    made changes

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -13,54 +13,8 @@ fun main(){
 
 
     }
-//    var benz = Car("Mercedes Benz",  "E300","KCC 153T" ,0)
-//    var toyota = Car("Toyota","Hilux","KCX 456",0)
-//        benz.start()
-//        println(benz.speed)
-//        benz.accelerates(45)
-//        benz.accelerates(72)
-//        print(benz.speed)
-//        benz.decelerates(-20)
-//      var mybook = Book("Born a crime" , "Trevor Noah" , 300,"John Maury")
-//          println(mybook.title)
-//          println(mybook.author)
-////
-//
-//
-//
-//
-//}
-//class Car(var make: String , var model: String, var registration:String ,var speed:Int){
-// fun start(){
-//     println("vroom vroom")
-// }
-//fun accelerates(acceleration:Int) :Int{
-//    speed += acceleration
-//    return speed
-//
-//     }
-// fun decelerates(deceleration:Int) :Int {
-//     speed -=deceleration
-//     return speed
-//       }
-//}
-//fun hoot(){
-//    println("piiiiiiii")
-//}
-//
-//fun stop(stop:Int) :Int{
 
 
-//data class Book(var title:String, var author:String,var pages:Int ,var publisher:String){
-//
-//data class Agestats(var maxAge:Int ,var minAge:String ,var avgAge :Double)
-//
-//fun calculate(age: Array<Int>): Agestats{
-//    val min= age.min()
-//    val max= age.max()
-//    val avg= age.average()
-//    val stats = Agestats(max, min ,avg)
-//    return stats
 
 
 class Human(var name:String, var age:Int ,var weight:Int){
@@ -69,10 +23,7 @@ class Human(var name:String, var age:Int ,var weight:Int){
         println("I am eating $foodweight kgs of food ")
     }
 
-//    companion object {
-//        fun speak(s: String) {
-//
-//        }
+
     fun speak(speech:String){
         println(speech)
     }
